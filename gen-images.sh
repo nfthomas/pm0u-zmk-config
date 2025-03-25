b6ba78f5cba7e1a11716f91d37cba051f3a9a2d9
nfthomas <70170157+nfthomas@users.noreply.github.com>
2025-03-25 16:21:43 -0700

    Use local img gen with venv

--- gen-images.sh
+++ gen-images.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 keymap -c ./keymap_drawer.config.yaml parse -z ./config/corne.keymap > draw/corne.yaml
-keymap -c ./keymap_drawer.config.yaml draw draw/corne.yaml -k "crkbd/rev4_1/mini" -l "LAYOUT_split_3x5_3" --keys-only > ./draw/corne.svg
-keymap -c ./keymap_drawer.config.yaml draw draw/corne.yaml -k "crkbd/rev4_1/mini" -l "LAYOUT_split_3x5_3" -s base --combos-only> ./draw/combos.svg
+# keymap -c ./keymap_drawer.config.yaml draw draw/corne.yaml -k "crkbd/rev4_1/mini" -l "LAYOUT_split_3x5_3" --keys-only > ./draw/corne.svg
+keymap -c ./keymap_drawer.config.yaml draw draw/corne.yaml -k "crkbd/rev1" -l "LAYOUT_split_3x6_3" --keys-only > ./draw/corne.svg
+keymap -c ./keymap_drawer.config.yaml draw draw/corne.yaml -k "crkbd/rev1" -l "LAYOUT_split_3x6_3" -s base --combos-only> ./draw/combos.svg
